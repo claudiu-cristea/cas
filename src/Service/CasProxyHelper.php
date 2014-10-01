d643ba2f0c583dd8cfc554e68438a5b3dfe43444
Tim Yale <yalet@lafayette.edu>
2014-10-01 10:23:16 -0400

    Started implementing proxy API in CasProxyHelper class.

--- src/Service/CasProxyHelper.php
+++ src/Service/CasProxyHelper.php
@@ -5,6 +5,7 @@ namespace Drupal\cas\Service;
 use Drupal\Core\Http\Client;
 use GuzzleHttp\Exception\ClientException;
 use Drupal\Component\Utility\Urlhelper;
+use Drupal\cas\Exception\CasValidateException;
 
 class CasProxyHelper {
 
@@ -52,15 +53,63 @@ class CasProxyHelper {
    *
    * @param string $target_service
    *   The service to be proxied.
+   * @return boolean
+   *   TRUE if proxy authenticated, FALSE otherwise.
    */
   public function proxyAuthenticate($target_service) {
     if (!($this->casHelper->isProxy() && isset($_SESSION['cas_pgt'])) {
       // We can't perform proxy authentication in this state.
-      // @TODO: We should throw some exception here.
+      return FALSE;
     }
     else {
       $url = $this->getServerProxyURL($target_service);
       // Need to actually make the request and parse it.
+      try {
+        $response = $this->httpClient->get($url);
+      }
+      catch (ClientException $e) {
+        return FALSE;
+      }
+      $proxy_ticket = $this->parseProxyTicket($response);
+      if (!$proxy_ticket) {
+        return FALSE;
+      }
+
     }
+  }
+
+  /**
+   * Parse proxy ticket from CAS Server response.
+   *
+   * @param string $xml
+   *   XML response from CAS Server.
+   * @return string
+   *   A proxy ticket to be used with the target service.
+   */
+  private function parseProxyTicket($xml) {
+    $dom = new \DomDocument();
+    $dom->preserveWhiteSpace = FALSE;
+    $dom->encoding = "utf-8";
+
+    if ($dom->loadXML($xml) === FALSE) {
+      return FALSE;
+    }
+    $failure_elements = $dom->getElementsByTagName("proxyFailure");
+    if ($failure_elements->length > 0) {
+      // Something went wrong with proxy ticket validation.
+      return FALSE;
+    }
+    $success_elements = $dom->getElementsByTagName("proxySuccess");
+    if ($success_elements->length === 0) {
+      // Malformed response from CAS Server.
+      return FALSE;
+    }
+    $success_element = $success_elements->item(0);
+    $proxy_ticket = $success_element->getElementsByTagName("proxyTicket");
+    if ($proxy_ticket->length === 0) {
+      // Malformed ticket.
+      return FALSE;
+    }
+    return $proxy_ticket->item(0)->nodeValue;
   }
 }
