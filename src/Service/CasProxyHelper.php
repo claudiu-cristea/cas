9dc19beab44628b640a5e775519a88ae0586c758
Tim Yale <yalet@lafayette.edu>
2014-10-03 10:37:56 -0400

    CasProxyHelper class for proxy authing to other services.

--- src/Service/CasProxyHelper.php
+++ src/Service/CasProxyHelper.php
@@ -6,6 +6,7 @@ use Drupal\Core\Http\Client;
 use GuzzleHttp\Exception\ClientException;
 use Drupal\Component\Utility\Urlhelper;
 use Drupal\cas\Exception\CasValidateException;
+use GuzzleHttp\Cookie\CookieJar;
 
 class CasProxyHelper {
 
@@ -43,7 +44,7 @@ class CasProxyHelper {
   private function getServerProxyURL($target_service) {
     $url = $this->casHelper->getServerBaseUrl() . 'proxy';
     $params = array();
-    $params['pgt'] = $_SESSION['cas_pgt'];
+    $params['pgt'] = $_SESSION['cas_pgt']->pgt;
     $params['targetService'] = $target_service;
     return $url . '?' . UrlHelper::buildQuery($params);
   }
@@ -51,30 +52,42 @@ class CasProxyHelper {
   /**
    * Proxy authenticates to a target service.
    *
+   * Returns cookies from the proxied service in a 
+   * CookieJar object for use when later accessing resources.
+   *
    * @param string $target_service
    *   The service to be proxied.
-   * @return boolean
-   *   TRUE if proxy authenticated, FALSE otherwise.
+   * @return mixed
+   *   \GuzzleHttp\Cookie\CookieJar if proxy auth success, FALSE otherwise.
    */
   public function proxyAuthenticate($target_service) {
-    if (!($this->casHelper->isProxy() && isset($_SESSION['cas_pgt'])) {
+    if (!($this->casHelper->isProxy() && isset($_SESSION['cas_pgt']))) {
       // We can't perform proxy authentication in this state.
       return FALSE;
     }
     else {
-      $url = $this->getServerProxyURL($target_service);
+      $cas_url = $this->getServerProxyURL($target_service);
       // Need to actually make the request and parse it.
       try {
-        $response = $this->httpClient->get($url);
+        $response = $this->httpClient->get($cas_url);
       }
       catch (ClientException $e) {
         return FALSE;
       }
-      $proxy_ticket = $this->parseProxyTicket($response);
+      $proxy_ticket = $this->parseProxyTicket($response->getBody());
       if (!$proxy_ticket) {
         return FALSE;
       }
-
+      $params['ticket'] = $proxy_ticket;
+      $service_url = $target_service . "?" . UrlHelper::buildQuery($params);
+      $cookieJar = new CookieJar();
+      try {
+        $data = $this->httpClient->get($service_url, ['cookies' => $cookieJar]);
+      }
+      catch (ClientException $e) {
+        return FALSE;
+      }
+      return $cookieJar;
     }
   }
 
@@ -83,14 +96,13 @@ class CasProxyHelper {
    *
    * @param string $xml
    *   XML response from CAS Server.
-   * @return string
-   *   A proxy ticket to be used with the target service.
+   * @return mixed
+   *   A proxy ticket to be used with the target service, FALSE on failure.
    */
   private function parseProxyTicket($xml) {
     $dom = new \DomDocument();
     $dom->preserveWhiteSpace = FALSE;
     $dom->encoding = "utf-8";
-
     if ($dom->loadXML($xml) === FALSE) {
       return FALSE;
     }
